fc96d8dda42dd93bfca8f6f8ec1f6988839019db
Andi Illa Erviani Nensi <89200106+Andiilla@users.noreply.github.com>
2023-07-03 11:46:29 +0800

    file r assignment

---
+++ M11 Time Series Non Musiman/Andi Illa.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(tseries)
+library(lmtest)
+library(normtest)
+library(nortest)
+
+
+setwd("C:/Users/adminstrator/Downloads/ANDI ILLA ERVIANI NENSI/KMMI DS02/data")
+data <-read.csv()
